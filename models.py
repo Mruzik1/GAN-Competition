f91b5f552b9a132a704c93293ac8e3ad04a9d0c1
Michael Ruzmetov <mykhailo.ruzmetov@student.tuke.sk>
2024-11-16 11:23:46 +0100

    new architecture test

--- models.py
+++ models.py
@@ -5,101 +5,202 @@ from torch import optim
 from pytorch_lightning import LightningModule
 
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(ResidualBlock, self).__init__()
-        
-        # no bias cause we have batch norm
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
-    
+class Upsample(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1, dropout=True):
+        super(Upsample, self).__init__()
+        self.dropout = dropout
+        self.block = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=nn.InstanceNorm2d),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+        self.dropout_layer = nn.Dropout2d(0.5)
+
+    def forward(self, x, shortcut=None):
+        x = self.block(x)
+        if self.dropout:
+            x = self.dropout_layer(x)
+
+        if shortcut is not None:
+            x = torch.cat([x, shortcut], dim=1)
+
+        return x
+
+
+class Downsample(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1, apply_instancenorm=True):
+        super(Downsample, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=nn.InstanceNorm2d)
+        self.norm = nn.InstanceNorm2d(out_channels)
+        self.relu = nn.LeakyReLU(0.2, inplace=True)
+        self.apply_norm = apply_instancenorm
+
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        
-        out = self.conv2(out)
-        out = self.bn2(out)
-        
-        out += self.shortcut(x)
-        out = self.relu(out)
-        
-        return out
-    
+        x = self.conv(x)
+        if self.apply_norm:
+            x = self.norm(x)
+        x = self.relu(x)
+
+        return x
+
 
 class GeneratorGAN(nn.Module):
-    def __init__(self):
+    def __init__(self, filter=64):
         super(GeneratorGAN, self).__init__()
+        self.downsamples = nn.ModuleList([
+            Downsample(3, filter, kernel_size=4, apply_instancenorm=False),  # (b, filter, 128, 128)
+            Downsample(filter, filter * 2),  # (b, filter * 2, 64, 64)
+            Downsample(filter * 2, filter * 4),  # (b, filter * 4, 32, 32)
+            Downsample(filter * 4, filter * 8),  # (b, filter * 8, 16, 16)
+            Downsample(filter * 8, filter * 8), # (b, filter * 8, 8, 8)
+            Downsample(filter * 8, filter * 8), # (b, filter * 8, 4, 4)
+            Downsample(filter * 8, filter * 8), # (b, filter * 8, 2, 2)
+        ])
 
-        self.relu = nn.ReLU(inplace=True)
-        self.tanh = nn.Tanh()
-        # self.max_pool = nn.MaxPool2d(kernel_size=2)
+        self.upsamples = nn.ModuleList([
+            Upsample(filter * 8, filter * 8),
+            Upsample(filter * 16, filter * 8),
+            Upsample(filter * 16, filter * 8),
+            Upsample(filter * 16, filter * 4, dropout=False),
+            Upsample(filter * 8, filter * 2, dropout=False),
+            Upsample(filter * 4, filter, dropout=False)
+        ])
 
-        # encoder
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=2)
+        self.last = nn.Sequential(
+            nn.ConvTranspose2d(filter * 2, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh()
+        )
 
-        # transformation with res blocks
-        self.res1 = ResidualBlock(256, 256, stride=1)
-        self.res2 = ResidualBlock(256, 256, stride=1)
-        self.res3 = ResidualBlock(256, 256, stride=1)
-        self.res4 = ResidualBlock(256, 256, stride=1)
-        
-        # decoder
-        self.trans_conv1 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, output_padding=1)
-        self.trans_conv2 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, output_padding=1)
-        self.conv4 = nn.Conv2d(64, 3, kernel_size=7, stride=1)
-    
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
+        skips = []
+        for l in self.downsamples:
+            x = l(x)
+            skips.append(x)
 
-        x = self.res1(x)
-        x = self.res2(x)
-        x = self.res3(x)
-        
-        x = self.relu(self.trans_conv1(x))
-        x = self.relu(self.trans_conv2(x))
-        x = self.tanh(self.conv4(x))
+        skips = reversed(skips[:-1])
+        for l, s in zip(self.upsamples, skips):
+            x = l(x, s)
 
-        return x
-    
+        out = self.last(x)
+
+        return out
+
 
 class DiscriminatorPatchGAN(nn.Module):
-    def __init__(self):
+    def __init__(self, filter=64):
         super(DiscriminatorPatchGAN, self).__init__()
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=nn.InstanceNorm2d)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=nn.InstanceNorm2d)
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=2, bias=nn.InstanceNorm2d)
-        self.conv4 = nn.Conv2d(256, 1, kernel_size=4, stride=1)
+        self.block = nn.Sequential(
+            Downsample(3, filter, kernel_size=4, stride=2, apply_instancenorm=False),
+            Downsample(filter, filter * 2, kernel_size=4, stride=2),
+            Downsample(filter * 2, filter * 4, kernel_size=4, stride=2),
+            Downsample(filter * 4, filter * 8, kernel_size=4, stride=1),
+        )
 
-        self.sigmoid = nn.Sigmoid()
-        self.leaky_relu = nn.LeakyReLU(0.2, inplace=True)
-
-        self.instance_norm1 = nn.InstanceNorm2d(64)
-        self.instance_norm2 = nn.InstanceNorm2d(128)
-        self.instance_norm3 = nn.InstanceNorm2d(256)
+        self.last = nn.Conv2d(filter * 8, 1, kernel_size=4, stride=1, padding=1)
 
     def forward(self, x):
-        x = self.leaky_relu(self.instance_norm1(self.conv1(x)))
-        x = self.leaky_relu(self.instance_norm2(self.conv2(x)))
-        x = self.leaky_relu(self.instance_norm3(self.conv3(x)))
-        x = self.sigmoid(self.conv4(x))
+        x = self.block(x)
+        x = self.last(x)
+
         return x
+
+
+# class ResidualBlock(nn.Module):
+#     def __init__(self, in_channels, out_channels, stride=1):
+#         super(ResidualBlock, self).__init__()
+        
+#         # no bias cause we have batch norm
+#         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(out_channels)
+#         self.relu = nn.ReLU(inplace=True)
+        
+#         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+#         self.bn2 = nn.BatchNorm2d(out_channels)
+        
+#         self.shortcut = nn.Sequential()
+#         if stride != 1 or in_channels != out_channels:
+#             self.shortcut = nn.Sequential(
+#                 nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+#                 nn.BatchNorm2d(out_channels)
+#             )
+    
+#     def forward(self, x):
+#         out = self.conv1(x)
+#         out = self.bn1(out)
+#         out = self.relu(out)
+        
+#         out = self.conv2(out)
+#         out = self.bn2(out)
+        
+#         out += self.shortcut(x)
+#         out = self.relu(out)
+        
+#         return out
+    
+
+# class GeneratorGAN(nn.Module):
+#     def __init__(self):
+#         super(GeneratorGAN, self).__init__()
+
+#         self.relu = nn.ReLU(inplace=True)
+#         self.tanh = nn.Tanh()
+#         # self.max_pool = nn.MaxPool2d(kernel_size=2)
+
+#         # encoder
+#         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=1)
+#         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
+#         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=2)
+
+#         # transformation with res blocks
+#         self.res1 = ResidualBlock(256, 256, stride=1)
+#         self.res2 = ResidualBlock(256, 256, stride=1)
+#         self.res3 = ResidualBlock(256, 256, stride=1)
+#         self.res4 = ResidualBlock(256, 256, stride=1)
+        
+#         # decoder
+#         self.trans_conv1 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, output_padding=1)
+#         self.trans_conv2 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, output_padding=1)
+#         self.conv4 = nn.Conv2d(64, 3, kernel_size=7, stride=1)
+    
+#     def forward(self, x):
+#         x = self.relu(self.conv1(x))
+#         x = self.relu(self.conv2(x))
+#         x = self.relu(self.conv3(x))
+
+#         x = self.res1(x)
+#         x = self.res2(x)
+#         x = self.res3(x)
+        
+#         x = self.relu(self.trans_conv1(x))
+#         x = self.relu(self.trans_conv2(x))
+#         x = self.tanh(self.conv4(x))
+
+#         return x
+    
+
+# class DiscriminatorPatchGAN(nn.Module):
+#     def __init__(self):
+#         super(DiscriminatorPatchGAN, self).__init__()
+
+#         self.conv1 = nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=nn.InstanceNorm2d)
+#         self.conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=nn.InstanceNorm2d)
+#         self.conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=2, bias=nn.InstanceNorm2d)
+#         self.conv4 = nn.Conv2d(256, 1, kernel_size=4, stride=1)
+
+#         self.sigmoid = nn.Sigmoid()
+#         self.leaky_relu = nn.LeakyReLU(0.2, inplace=True)
+
+#         self.instance_norm1 = nn.InstanceNorm2d(64)
+#         self.instance_norm2 = nn.InstanceNorm2d(128)
+#         self.instance_norm3 = nn.InstanceNorm2d(256)
+
+#     def forward(self, x):
+#         x = self.leaky_relu(self.instance_norm1(self.conv1(x)))
+#         x = self.leaky_relu(self.instance_norm2(self.conv2(x)))
+#         x = self.leaky_relu(self.instance_norm3(self.conv3(x)))
+#         x = self.sigmoid(self.conv4(x))
+#         return x
 
 
 class CycleGAN(LightningModule):
